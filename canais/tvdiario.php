4f205db85e681d11cab4141e7d446b575d0bc798
maketvbr <96959793+Emanueldelima@users.noreply.github.com>
2022-11-18 14:39:16 -0300

    Update tvdiario.php

--- canais/tvdiario.php
+++ canais/tvdiario.php
@@ -7,11 +7,11 @@ $page=file_get_contents('http://www.dailymotion.com/embed/video/'.$videoid);
 $link = substr($page, strlen('mp4","url":"')+strripos($page, 'mp4","url":"'));  // beginning of link
 $link = substr($link, 0, strpos($link, '"}'));  // end of link
 $link = str_replace("\\", '', $link);   // delete backslashes\
-$path = $path_download_file.$videoid.".mp4";
+$path = $path_download_file.$videoid.".m3u";
 
 if ((!file_exists($path)) && (!empty($link))){
   file_put_contents($path_log_link.$videoid.'.txt', $link); // save link in log
-  $cmd='wget -bqc '.$link.' -O "'.$path_download_file.$videoid.'.mp4"';
+  $cmd='wget -bqc '.$link.' -O "'.$path_download_file.$videoid.'.m3u"';
   exec($cmd,$o,$r);
 }
 ?>
