b439545ef1413d6dbfa9f16cacb07cf65687911f
maketvbr <96959793+Emanueldelima@users.noreply.github.com>
2022-11-18 14:23:38 -0300

    Create tvdiario.php

---
+++ tvdiario.php
@@ -0,0 +1,17 @@
+<?php
+$videoid="x62w2zn";//a static dailymotion video id
+$path_log_link='logs/';
+$path_download_file='videos/';
+$link="";
+$page=file_get_contents('http://www.dailymotion.com/embed/video/'.$videoid);
+$link = substr($page, strlen('mp4","url":"')+strripos($page, 'mp4","url":"'));  // beginning of link
+$link = substr($link, 0, strpos($link, '"}'));  // end of link
+$link = str_replace("\\", '', $link);   // delete backslashes\
+$path = $path_download_file.$videoid.".mp4";
+
+if ((!file_exists($path)) && (!empty($link))){
+  file_put_contents($path_log_link.$videoid.'.txt', $link); // save link in log
+  $cmd='wget -bqc '.$link.' -O "'.$path_download_file.$videoid.'.mp4"';
+  exec($cmd,$o,$r);
+}
+?>
